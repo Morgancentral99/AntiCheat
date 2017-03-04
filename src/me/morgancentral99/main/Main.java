c4a67f8203350cf9ea35c949a50e402815154f67
Morgancentral99 <morgancentral99@gmail.com>
2017-03-04 09:57:25 -0700

    Added Main

---
+++ src/me/morgancentral99/main/Main.java
@@ -0,0 +1,14 @@
+package me.morgancentral99.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+
+}
